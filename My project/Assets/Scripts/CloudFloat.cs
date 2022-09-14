6e3dfd4ef6469bbb3b861c41175dce6358135823
Redworm7 <jawallis19@gmail.com>
2022-09-14 17:18:33 -0400

    Everything
    Everything in this project up until now

---
+++ My project/Assets/Scripts/CloudFloat.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudFloat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Hello world");
+        Debug.Log(this.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Frame");
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + (0.1f * Time.deltaTime));
+    }
+}
